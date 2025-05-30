409443553b68839cc87e5d79dc8f0dfd9e9cb40c
Alexander Samaniego <alex_samaniego96@hotmail.com>
2025-05-29 21:50:01 -0400

    Initial Commit

---
+++ MyHockeyApp/MyHockeyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHockeyAppApp.swift
+//  MyHockeyApp
+//
+//  Created by Alex Samaniego on 2025-05-29.
+//
+
+import SwiftUI
+
+@main
+struct MyHockeyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyHockeyAppTests/MyHockeyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyHockeyAppTests.swift
+//  MyHockeyAppTests
+//
+//  Created by Alex Samaniego on 2025-05-29.
+//
+
+import Testing
+@testable import MyHockeyApp
+
+struct MyHockeyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
